254ad95965fdf4d070224d0ac5d223186a69d0ef
matthiasHsieh <matthiasxywilliams@gmail.com>
2021-11-24 08:35:12 +0800

    Create ruihualvbo1.m
    Add the Sobel filtering of gradient.

---
+++ sobel-filtering/ruihualvbo1.m
@@ -0,0 +1,32 @@
+%% Read in image 
+I = imread('guniang.png');
+% yunannan.png
+% xiaoguan.png
+% tangrenjie.png
+% shilichongxiao.png
+% guniang.png
+
+%% Convert to grayscale image
+Igray = rgb2gray(I);
+
+%% Options
+% Sharpening filter
+% Robert gradient
+% Sobel gradient
+sw1 = fspecial('sobel');
+sw2 = fspecial('sobel');
+Horizontal = imfilter(Igray,sw1);
+vertical = imfilter(Igray,sw2);
+Isobel = abs(Horizontal)+abs(vertical);
+
+%% Display the image 
+
+subplot(2,2,1);
+    imshow(Igray); 
+    title('Grayscale Image');
+subplot(2,2,2);
+    imshow(Horizontal,[]),title('Horizontal sobel');  
+subplot(2,2,3);
+    imshow(vertical,[]),title('vertical sobel');
+subplot(2,2,4);
+    imshow(Isobel,[]),title('sobel');
