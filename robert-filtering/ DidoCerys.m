cebf702fb6502330836226d6be6778a944d86d7d
matthiasHsieh <matthiasxywilliams@gmail.com>
2021-11-27 09:38:49 +0800

    Update  DidoCerys.m
    Change the picture to PenkaAlastair.jpg and minus some empty lines.

--- robert-filtering/ DidoCerys.m
+++ robert-filtering/ DidoCerys.m
@@ -1,10 +1,5 @@
 %% Read in image 
-I = imread('guniang.png');
-% yunannan.png
-% xiaoguan.png
-% tangrenjie.png
-% shilichongxiao.png
-% guniang.png
+I = imread('PenkaAlastair.jpg');
 
 %% Convert to grayscale image
 Igray = rgb2gray(I);
