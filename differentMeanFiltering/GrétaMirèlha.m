74ba5a934b05eb14a35146f016cc6eccfddc3396
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-11-27 11:31:22 +0800

    Update and rename xianxingpinghualc0.m to GrétaMirèlha.m
    Change picture to MelindaŽaklina.jpg and minus some empty lines.

--- differentMeanFiltering/GrétaMirèlha.m
+++ differentMeanFiltering/GrétaMirèlha.m
@@ -1,10 +1,5 @@
 %% Read in image 
-I = imread('shilichongxiao.png');
-% yunannan.png
-% xiaoguan.png
-% tangrenjie.png
-% shilichongxiao.png
-% guniang.png
+I = imread('MelindaŽaklina.jpg');
 
 %% Convert to grayscale image
 Igray = rgb2gray(I);
