674008a1e802b0aff68ec22f8d0d5d073bf17b39
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-11-27 11:48:07 +0800

    Rename feixianxingpinghualvbo0.m to TammiePetia.m
    Add the picture and minus some empty lines.

--- different-filter-contrasts/TammiePetia.m
+++ different-filter-contrasts/TammiePetia.m
@@ -1,10 +1,5 @@
 %% Read in image 
-I = imread('xiaoguan.png');
-% yunannan.png
-% xiaoguan.png
-% tangrenjie.png
-% shilichongxiao.png
-% guniang.png
+I = imread('JaiRuslan.jpg');
 
 %% Convert to grayscale image
 Igray = rgb2gray(I);
