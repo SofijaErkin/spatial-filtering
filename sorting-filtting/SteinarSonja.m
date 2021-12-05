5b606b4f69db821a5a232ad874cf318ba154931e
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-12-05 09:59:10 +0800

    Update SteinarSonja.m
    Minus some empty lines.

--- sorting-filtting/SteinarSonja.m
+++ sorting-filtting/SteinarSonja.m
@@ -1,12 +1,9 @@
 %% Read in image 
 I = imread('SámuelTitus.jpg');
-
 %% Convert to grayscale image
 Igray = rgb2gray(I);
-
 %% Add noise to image
 Islat_pepper = imnoise(I, 'salt & pepper', 0.0001);
-
 %% Operator
 % Maximum filter salt & pepper noise  
 Imax = imdilate(I, true(3));
@@ -17,9 +14,7 @@ Imin = imerode(I, true(3));
 %imshow(Imin, []);
 %    title('Mininum filter salt & pepper');
 %    xlabel('density 0.15');
-
 %% Diaplay the image 
-
 subplot(1, 3, 1),
 imshow(I);
     title('salt & pepper'); 
