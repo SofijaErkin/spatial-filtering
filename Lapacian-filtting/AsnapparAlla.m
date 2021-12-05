0ccaf0d169f49646e02fc5d520e79e2baf7eb459
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-12-05 10:08:22 +0800

    Update AsnapparAlla.m
    Minus some empty lines.

--- Lapacian-filtting/AsnapparAlla.m
+++ Lapacian-filtting/AsnapparAlla.m
@@ -1,9 +1,7 @@
 %% Read in image 
 I = imread('BrigitaRomey.jpg');
-
 %% Convert to grayscale image
 Igray = rgb2gray(I);
-
 %% Options
 % Lapacian gradient
 lapw1 = [0 -1 0;-1 4 -1;0 -1 0];
@@ -12,9 +10,7 @@ lapw2 = [-1 -1 -1;-1 8 -1 ;-1 -1 -1];
 Lap2 = imfilter(Igray, lapw2,'corr','replicate');
 lapw3 = [1 4 1;4 -20 4;1 4 1];
 Lap3 = imfilter(Igray, lapw3,'corr','replicate');
-
 %% Display the image 
-
 subplot(2,2,1);
 imshow(Igray); 
     title('Grayscale Image');
