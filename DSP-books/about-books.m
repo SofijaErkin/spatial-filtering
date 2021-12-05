5b850bc2855565e89cafd6e622864d68134cc0aa
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-12-05 10:09:53 +0800

    Update about-books.m
    Add a way to get the ebook.

--- DSP-books/about-books.m
+++ DSP-books/about-books.m
@@ -1,2 +1,5 @@
 % Digital Image Processing Third Edition Chinese Version Gonzalez
+
 % You konw, it's to big size 88MB than 25MB requried by Github. What's amazing!
+
+% It's better for you to download the ebook via Zlibrary.
