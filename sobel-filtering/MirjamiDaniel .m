49e343db6d187578bb08a78b8bcd659e13fe0ceb
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-12-05 10:00:48 +0800

    Update MirjamiDaniel .m
    Minus some empty lines.

--- sobel-filtering/MirjamiDaniel .m
+++ sobel-filtering/MirjamiDaniel .m
@@ -1,9 +1,7 @@
 %% Read in image 
 I = imread('RostomParthenia.jpg');
-
 %% Convert to grayscale image
 Igray = rgb2gray(I);
-
 %% Options
 % Sharpening filter
 % Robert gradient
@@ -13,9 +11,7 @@ sw2 = fspecial('sobel');
 Horizontal = imfilter(Igray,sw1);
 vertical = imfilter(Igray,sw2);
 Isobel = abs(Horizontal)+abs(vertical);
-
 %% Display the image 
-
 subplot(2,2,1);
     imshow(Igray); 
     title('Grayscale Image');
